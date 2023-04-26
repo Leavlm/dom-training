ccb37c6b4ae1c0d83ee7dbd59ec346aa3e046427
Léa Valmy <lea.valmy@gmail.com>
2023-04-26 16:55:29 +0200

    exo 4

--- js/training.js
+++ js/training.js
@@ -1,21 +1,63 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
+function getHidden(event) {
+    const text = document.querySelector("#disappearing");
+    text.classList.toggle("bye-text");
+}
 
+document.querySelector("#buttonToHide").addEventListener("click", getHidden);
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
 
+function scroll(event){
+    document.querySelector("#ex2-scroll-value").textContent = window.scrollY}
+
+window.addEventListener("scroll", scroll)
+
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
 
+const animalList = document.querySelector("#ex3-animals");
+const animalButton = document.querySelector("#changeAnimals")
+
+function moveAnimals(event){
+    animalList.appendChild(animalList.firstElementChild)
+}
+
+document.querySelector("#changeAnimals").addEventListener("click", moveAnimals)
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
 
+function changeColor(event) {
+    let btn = document.querySelector("#buttonToChange")
+    if (btn.classList.contains("blue")){
+        btn.classList.remove("blue");
+        btn.classList.add("red");
+    }
+    else if (btn.classList.contains("red")){
+        btn.classList.remove("red");
+        btn.classList.add("green")
+    }
+    else{
+        btn.classList.remove("green")
+        btn.classList.add("blue");
+    }
+}
+
+document.querySelector("#buttonToChange").addEventListener("click", changeColor);
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
+
+function changeBackground(event){
+    let emojiList = document.querySelector('#emojiList');
+    
+}
+
+document.querySelector("#emojiList").addEventListener("hover", changeBackground)
 
 
 /* ------------------------------------ */
